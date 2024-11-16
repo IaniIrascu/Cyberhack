1a346ef95f06625644fdcd7812c7616d4f4e9109
devidd22 <mihail_devid.mecu@stud.acs.upb.ro>
2024-11-16 16:11:53 +0200

    F2 button

--- Cyberhack/Form1.cs
+++ Cyberhack/Form1.cs
@@ -1,9 +1,21 @@
-namespace Cyberhack;
+using Microsoft.Win32;
 
-public partial class Form1 : Form
+namespace Cyberhack
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+            AddApplicationToStartup();
+        }
+
+        private void AddApplicationToStartup()
+        {
+            string appName = "MyApp";
+            string appPath = Application.ExecutablePath;
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            rk.SetValue(appName, appPath);
+        }
     }
 }
--- Cyberhack/obj/Debug/net9.0-windows/Cyberhack.AssemblyInfo.cs
+++ Cyberhack/obj/Debug/net9.0-windows/Cyberhack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cyberhack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ea9e1aa56d7d1b4a9fb29fb4730cd8615b6c79c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd804f5b02497c68d03ae5e1ff11833c30d78d8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cyberhack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cyberhack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
