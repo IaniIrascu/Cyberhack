4e304e8b21ec39e7d304d229d590793684038232
Eugen2613 <cantaragiu.eeugen@gmail.com>
2024-11-16 21:57:06 +0200

    habar nu am

--- Cyberhack/Form1.Designer.cs
+++ Cyberhack/Form1.Designer.cs
@@ -29,53 +29,89 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        components = new System.ComponentModel.Container();
         button1 = new System.Windows.Forms.Button();
-        bindingSource1 = new System.Windows.Forms.BindingSource(components);
-        textBox2 = new System.Windows.Forms.TextBox();
-        ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+        textBox1 = new System.Windows.Forms.TextBox();
+        button2 = new System.Windows.Forms.Button();
+        button3 = new System.Windows.Forms.Button();
+        label1 = new System.Windows.Forms.Label();
         SuspendLayout();
         // 
         // button1
         // 
-        button1.Location = new System.Drawing.Point(311, 73);
+        button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+        button1.Location = new System.Drawing.Point(580, 188);
         button1.Name = "button1";
-        button1.Size = new System.Drawing.Size(119, 88);
+        button1.Size = new System.Drawing.Size(179, 45);
         button1.TabIndex = 0;
-        button1.Text = "button1";
-        button1.UseVisualStyleBackColor = true;
+        button1.Text = "Brighter";
+        button1.UseVisualStyleBackColor = false;
         button1.Click += button1_Click;
         // 
-        // bindingSource1
+        // textBox1
         // 
-        bindingSource1.CurrentChanged += bindingSource1_CurrentChanged;
+        textBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+        textBox1.Location = new System.Drawing.Point(65, 330);
+        textBox1.Name = "textBox1";
+        textBox1.Size = new System.Drawing.Size(471, 27);
+        textBox1.TabIndex = 3;
+        textBox1.TextChanged += textBox1_TextChanged;
         // 
-        // textBox2
+        // button2
         // 
-        textBox2.Location = new System.Drawing.Point(244, 200);
-        textBox2.Name = "textBox2";
-        textBox2.Size = new System.Drawing.Size(251, 27);
-        textBox2.TabIndex = 3;
+        button2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+        button2.Location = new System.Drawing.Point(65, 188);
+        button2.Name = "button2";
+        button2.Size = new System.Drawing.Size(179, 45);
+        button2.TabIndex = 4;
+        button2.Text = "Dimmer";
+        button2.UseVisualStyleBackColor = false;
+        button2.Click += button2_Click;
+        // 
+        // button3
+        // 
+        button3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+        button3.Location = new System.Drawing.Point(580, 330);
+        button3.Name = "button3";
+        button3.Size = new System.Drawing.Size(179, 45);
+        button3.TabIndex = 5;
+        button3.Text = "Search";
+        button3.UseVisualStyleBackColor = false;
+        button3.Click += button3_Click;
+        // 
+        // label1
+        // 
+        label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+        label1.Location = new System.Drawing.Point(275, 37);
+        label1.Name = "label1";
+        label1.Size = new System.Drawing.Size(277, 62);
+        label1.TabIndex = 6;
+        label1.Text = "Welcome to MyConfig\r\n";
+        label1.Click += label1_Click;
         // 
         // Form1
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         ClientSize = new System.Drawing.Size(800, 450);
-        Controls.Add(textBox2);
+        Controls.Add(label1);
+        Controls.Add(button3);
+        Controls.Add(button2);
+        Controls.Add(textBox1);
         Controls.Add(button1);
         Margin = new System.Windows.Forms.Padding(2);
-        Text = "Form1";
-        ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+        Text = "MyConfig";
         ResumeLayout(false);
         PerformLayout();
     }
 
-    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label1;
 
-    private System.Windows.Forms.BindingSource bindingSource1;
+    private System.Windows.Forms.Button button3;
 
+    private System.Windows.Forms.Button button2;
+
+    private System.Windows.Forms.TextBox textBox1;
+    
     private System.Windows.Forms.Button button1;
-
     #endregion
 }
--- Cyberhack/Form1.cs
+++ Cyberhack/Form1.cs
@@ -9,7 +9,6 @@ using System.Windows.Forms;
 using System.Diagnostics;
 using static Cyberhack.KeyWordFinder;
 using Google.Apis.Auth.OAuth2;
-
 /*namespace GoogleTranslate
 { 
     public class GoogleTranslate
@@ -49,11 +48,86 @@ using Google.Apis.Auth.OAuth2;
 */
 public partial class Form1 : Form
 {
+        private void CustomizeUI()
+        {
+            // Set modern font
+            Font modernFont = new Font("Segoe UI", 10, FontStyle.Regular);
+            // Style Button1 (Brighter)
+            
+            
+            label1.Font = modernFont;
+            label1.BackColor = Color.Black;
+            label1.FlatStyle = FlatStyle.Flat;
+            
+            label1.ForeColor = Color.White;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Text = "Welcome to my Config";
+            
+            button1.Font = modernFont;
+            button1.BackColor = Color.LightBlue;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.ForeColor = Color.White;
+            button1.TextAlign = ContentAlignment.MiddleCenter;
+            button1.Text = "Brighter ☀";
+
+            // Style Button2 (Dimmer)
+            button2.Font = modernFont;
+            button2.BackColor = Color.CornflowerBlue;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.ForeColor = Color.White;
+            button2.TextAlign = ContentAlignment.MiddleCenter;
+            button2.Text = "Dimmer 🌙";
+
+            // Style Button3 (Search)
+            button3.Font = modernFont;
+            button3.BackColor = Color.MediumSlateBlue;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.ForeColor = Color.White;
+            button3.TextAlign = ContentAlignment.MiddleCenter;
+            button3.Text = "Search 🔍";
+
+            // Style TextBox1 (Input)
+            textBox1.Font = modernFont;
+            textBox1.BackColor = Color.WhiteSmoke;
+            textBox1.ForeColor = Color.Gray;
+            textBox1.Text = "Type your query here...";
+            textBox1.GotFocus += RemovePlaceholderText;
+            textBox1.LostFocus += AddPlaceholderText;
+            
+            button1.Anchor = AnchorStyles.None;
+            button2.Anchor = AnchorStyles.None;
+            button3.Anchor = AnchorStyles.None;
+            textBox1.Anchor = AnchorStyles.None;
+            label1.Anchor = AnchorStyles.None;
+        }
+        
+        // Placeholder handling for the text box
+        private void RemovePlaceholderText(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Type your query here...")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void AddPlaceholderText(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                textBox1.Text = "Type your query here... ";
+                textBox1.ForeColor = Color.Gray;
+            }
+        }
+
     private void Form1_KeyDown(object sender, KeyEventArgs e) 
     {
         if (e.KeyCode == Keys.Enter)
         {
-            textBox2.Focus();
+            textBox1.Focus();
             button1.Focus();
             button1_Click(sender, e);
         }
@@ -63,6 +137,7 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+        CustomizeUI();
     }
 
     private void textBox2_TextChanged(object sender, KeyEventArgs e)
@@ -75,10 +150,15 @@ public partial class Form1 : Form
         throw new System.NotImplementedException();
     }
 
-    private void button1_Click(object sender, EventArgs e)
+    private void button3_Click(object sender, EventArgs e)
     {
+        string workingDirectory = Environment.CurrentDirectory;
+        string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        Console.WriteLine(projectDirectory);
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer(projectDirectory + "\\file_example_WAV_1MG.wav");
+        player.Play();
         WindowsSettingsBrightnessController.Set(100);
-        string input = textBox2.Text.ToLower();
+        string input = textBox1.Text.ToLower();
         // if(e.KeyCode==Keys.Enter)
         //     buttonSearch_Click(sender,e);
             // preiau textul din TextBox.
@@ -113,6 +193,8 @@ public partial class Form1 : Form
                 process.StartInfo.FileName = "ms-settings:";
                 process.StartInfo.UseShellExecute = true;
                 process.Start();
+                
+                // Creeaza un label
             }
             
         }
@@ -157,5 +239,24 @@ public partial class Form1 : Form
         
         // we have the keyword that the user type.
         // and now we apply the handlefunction for that keyword, keyword.
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {  
+        WindowsSettingsBrightnessController.Set(WindowsSettingsBrightnessController.Get() + 10);
+    }
+    private void button2_Click(object sender, EventArgs e)
+    {
+        WindowsSettingsBrightnessController.Set(WindowsSettingsBrightnessController.Get() - 10);
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+       
     }
 }
