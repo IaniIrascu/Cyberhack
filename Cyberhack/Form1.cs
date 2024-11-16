77b869ab4535a3587129a701172092e8a45ee712
devidd22 <mihail_devid.mecu@stud.acs.upb.ro>
2024-11-16 18:11:11 +0200

    F2 button

--- Cyberhack/Form1.cs
+++ Cyberhack/Form1.cs
@@ -1,81 +1,81 @@
-namespace Cyberhack;
-using System;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using System.Diagnostics;
-using static Cyberhack.KeyWordFinder;
-public partial class Form1 : Form
+namespace Cyberhack
 {
-    public Form1()
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Windows.Forms;
+    using System.IO;
+    using static Cyberhack.KeyWordFinder;
+
+    public partial class Form1 : Form
     {
-        InitializeComponent();
-    }
-
-    private void textBox2_TextChanged(object sender, KeyEventArgs e)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    private void bindingSource1_CurrentChanged(object sender, EventArgs e)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    private void button1_Click(object sender, EventArgs e)
-    {
-        string input = textBox2.Text;
-        // if(e.KeyCode==Keys.Enter)
-        //     buttonSearch_Click(sender,e);
-            // preiau textul din TextBox.
-
-        // creez instanta si caut substringurile.
-        KeyWordFinder finder = new KeyWordFinder();
-        
-        List<string> words = ["whatsapp", "desktop", "instagram", "chrome", "settings", "background", "word", "excel", "powerpoint", "gallery", "brightness", "files", "pictures", "documents"];
-
-        String keyword = finder.FindSubstring(input, words);
-        
-        // Cautare keyword principal
-
-        if (keyword == "settings")
+        public Form1()
         {
-            List<String> listaSetari = ["display", "camera", "keyboard", "language", "recovery",
-                "taskbar", "nightlight", "about", "sound", "apps-volume",
-                "printers"];
-
-            String keywordSettings = finder.FindSubstring(input, listaSetari);
-            if (keywordSettings != null)
-            {
-                Process process = new Process();
-                process.StartInfo.FileName = "ms-settings:" + keywordSettings;
-                process.StartInfo.UseShellExecute = true;
-                process.Start();
-            }
-            else
-            {
-                Process process = new Process();
-                process.StartInfo.FileName = "ms-settings:";
-                process.StartInfo.UseShellExecute = true;
-                process.Start();
-            }
-            
+            InitializeComponent();
         }
-        else if (keyword == "desktop")
-        {
-            IEnumerable<FileInfo> list = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)).GetFiles()
-                .Concat(new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.CommonDesktopDirectory)).GetFiles());
 
-            foreach (var file in list)
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox2.Text;
+            KeyWordFinder finder = new KeyWordFinder();
+
+            List<string> words = new List<string>
             {
-                if (input.Contains(file.Name))
+                "whatsapp", "desktop", "instagram", "chrome", "settings", "background",
+                "word", "excel", "powerpoint", "gallery", "brightness", "files",
+                "pictures", "documents"
+            };
+
+            string keyword = finder.FindSubstring(input, words);
+
+            if (keyword == "settings")
+            {
+                List<string> listaSetari = new List<string>
                 {
-                    Console.WriteLine(file.Name);
+                    "display", "camera", "keyboard", "language", "recovery",
+                    "taskbar", "nightlight", "about", "sound", "apps-volume",
+                    "printers"
+                };
+
+                string keywordSettings = finder.FindSubstring(input, listaSetari);
+                if (keywordSettings != null)
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = "ms-settings:" + keywordSettings;
+                    process.StartInfo.UseShellExecute = true;
+                    process.Start();
+                }
+                else
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = "ms-settings:";
+                    process.StartInfo.UseShellExecute = true;
+                    process.Start();
                 }
             }
-            //keyword = finder.FindSubstring(input, words);
+            else if (keyword == "desktop")
+            {
+                IEnumerable<FileInfo> list = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)).GetFiles()
+                    .Concat(new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.CommonDesktopDirectory)).GetFiles());
+
+                foreach (var file in list)
+                {
+                    if (input.Contains(file.Name))
+                    {
+                        Console.WriteLine(file.Name);
+                    }
+                }
+            }
         }
-        
-        // we have the keyword that the user type.
-        // and now we apply the handlefunction for that keyword, keyword.
     }
 }
