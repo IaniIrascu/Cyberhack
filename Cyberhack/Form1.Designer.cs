669b235197e27a18e287c03e5c952a63db8131ab
Eugen2613 <cantaragiu.eeugen@gmail.com>
2024-11-16 15:34:19 +0200

    main

--- Cyberhack/Form1.Designer.cs
+++ Cyberhack/Form1.Designer.cs
@@ -30,35 +30,38 @@ partial class Form1
     private void InitializeComponent()
     {
         button1 = new System.Windows.Forms.Button();
-        checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+        textBox1 = new System.Windows.Forms.TextBox();
         SuspendLayout();
         // 
         // button1
         // 
-        button1.Location = new System.Drawing.Point(418, 154);
+        button1.Location = new System.Drawing.Point(249, 207);
         button1.Name = "button1";
-        button1.Size = new System.Drawing.Size(119, 88);
+        button1.Size = new System.Drawing.Size(119, 37);
         button1.TabIndex = 0;
         button1.Text = "button1";
         button1.UseVisualStyleBackColor = true;
+        button1.Click += button1_Click;
         // 
-        // checkedListBox1
+        // textBox1
         // 
-        checkedListBox1.FormattingEnabled = true;
-        checkedListBox1.Location = new System.Drawing.Point(122, 175);
-        checkedListBox1.Name = "checkedListBox1";
-        checkedListBox1.Size = new System.Drawing.Size(170, 32);
-        checkedListBox1.TabIndex = 1;
+        textBox1.Location = new System.Drawing.Point(215, 174);
+        textBox1.Name = "textBox1";
+        textBox1.Size = new System.Drawing.Size(191, 27);
+        textBox1.TabIndex = 1;
+        textBox1.TextChanged += textBox1_TextChanged;
         // 
         // Form1
         // 
-        AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         ClientSize = new System.Drawing.Size(800, 450);
-        Controls.Add(checkedListBox1);
+        Controls.Add(textBox1);
         Controls.Add(button1);
+        Margin = new System.Windows.Forms.Padding(2);
         Text = "Form1";
         ResumeLayout(false);
+        PerformLayout();
     }
 
     private System.Windows.Forms.CheckedListBox checkedListBox1;
