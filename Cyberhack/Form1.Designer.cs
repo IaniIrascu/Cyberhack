4b5eea4afea4a9dc0f7ad7dc3abe4ab94924068d
Iani Irașcu <95540944+IaniIrascu@users.noreply.github.com>
2024-11-16 13:26:52 +0200

    Add files via upload

---
+++ Cyberhack/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Cyberhack;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        button1 = new System.Windows.Forms.Button();
+        checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+        SuspendLayout();
+        // 
+        // button1
+        // 
+        button1.Location = new System.Drawing.Point(418, 154);
+        button1.Name = "button1";
+        button1.Size = new System.Drawing.Size(119, 88);
+        button1.TabIndex = 0;
+        button1.Text = "button1";
+        button1.UseVisualStyleBackColor = true;
+        // 
+        // checkedListBox1
+        // 
+        checkedListBox1.FormattingEnabled = true;
+        checkedListBox1.Location = new System.Drawing.Point(122, 175);
+        checkedListBox1.Name = "checkedListBox1";
+        checkedListBox1.Size = new System.Drawing.Size(170, 32);
+        checkedListBox1.TabIndex = 1;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(checkedListBox1);
+        Controls.Add(button1);
+        Text = "Form1";
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.CheckedListBox checkedListBox1;
+
+    private System.Windows.Forms.Button button1;
+
+    #endregion
+}
