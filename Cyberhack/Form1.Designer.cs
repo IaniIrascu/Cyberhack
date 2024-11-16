d066651106839bb8bf96323d2ff39211e56ce167
Eugen2613 <cantaragiu.eeugen@gmail.com>
2024-11-16 16:00:22 +0200

    nu stiu

--- Cyberhack/Form1.Designer.cs
+++ Cyberhack/Form1.Designer.cs
@@ -29,42 +29,51 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
+        components = new System.ComponentModel.Container();
         button1 = new System.Windows.Forms.Button();
-        textBox1 = new System.Windows.Forms.TextBox();
+        bindingSource1 = new System.Windows.Forms.BindingSource(components);
+        textBox2 = new System.Windows.Forms.TextBox();
+        ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
         SuspendLayout();
         // 
         // button1
         // 
-        button1.Location = new System.Drawing.Point(249, 207);
+        button1.Location = new System.Drawing.Point(311, 73);
         button1.Name = "button1";
-        button1.Size = new System.Drawing.Size(119, 37);
+        button1.Size = new System.Drawing.Size(119, 88);
         button1.TabIndex = 0;
         button1.Text = "button1";
         button1.UseVisualStyleBackColor = true;
         button1.Click += button1_Click;
         // 
-        // textBox1
+        // bindingSource1
         // 
-        textBox1.Location = new System.Drawing.Point(215, 174);
-        textBox1.Name = "textBox1";
-        textBox1.Size = new System.Drawing.Size(191, 27);
-        textBox1.TabIndex = 1;
-        textBox1.TextChanged += textBox1_TextChanged;
+        bindingSource1.CurrentChanged += bindingSource1_CurrentChanged;
+        // 
+        // textBox2
+        // 
+        textBox2.Location = new System.Drawing.Point(244, 200);
+        textBox2.Name = "textBox2";
+        textBox2.Size = new System.Drawing.Size(251, 27);
+        textBox2.TabIndex = 3;
         // 
         // Form1
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         ClientSize = new System.Drawing.Size(800, 450);
-        Controls.Add(textBox1);
+        Controls.Add(textBox2);
         Controls.Add(button1);
         Margin = new System.Windows.Forms.Padding(2);
         Text = "Form1";
+        ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
         ResumeLayout(false);
         PerformLayout();
     }
 
-    private System.Windows.Forms.CheckedListBox checkedListBox1;
+    private System.Windows.Forms.TextBox textBox2;
+
+    private System.Windows.Forms.BindingSource bindingSource1;
 
     private System.Windows.Forms.Button button1;
 
--- Cyberhack/obj/Debug/net9.0-windows/Cyberhack.AssemblyInfo.cs
+++ Cyberhack/obj/Debug/net9.0-windows/Cyberhack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cyberhack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b5eea4afea4a9dc0f7ad7dc3abe4ab94924068d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669b235197e27a18e287c03e5c952a63db8131ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cyberhack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cyberhack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
