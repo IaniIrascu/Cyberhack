bcf3363c08167c4393a1c43dc2970511d614b666
Eugen2613 <cantaragiu.eeugen@gmail.com>
2024-11-17 06:05:32 +0200

    highlights

---
+++ Cyberhack/Form2.Designer.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+
+namespace Cyberhack;
+
+partial class Form2
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        // 
+        // Form2
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Text = "Form2";
+        ResumeLayout(false);
+    }
+
+    #endregion
+}
